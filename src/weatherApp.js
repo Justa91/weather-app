e709bee848192452bebc2eb0b0aa93293738d745
Justyna <jkkrupaaa@gmail.com>
2022-11-14 23:36:07 +0100

    updated the time

--- src/weatherApp.js
+++ src/weatherApp.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuseday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showWeather(response) {
   console.log(response.data);
   let temperature = document.querySelector("h2");
@@ -15,12 +38,14 @@ function showWeather(response) {
   let iconWeather = document.querySelector("#icon-weather");
   let currentIconWeather = response.data.condition.icon_url;
   iconWeather.setAttribute("src", currentIconWeather);
+  let date = document.querySelector("#date");
+  date.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "1t46b45ad73356d1fdb57b7o025ca415";
 let units = "metric";
 
-let city = "Greece";
+let city = "warsaw";
 
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
 
