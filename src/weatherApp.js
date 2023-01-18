f0fd148db1c00f159fd3d22f6ddff0e0c904cbee
Justyna <jkkrupaaa@gmail.com>
2023-01-18 22:33:02 +0100

    added forecast api

--- src/weatherApp.js
+++ src/weatherApp.js
@@ -21,32 +21,54 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatForcastDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector(".weather-forecast");
   let forecastHTML = `<div class = "row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
-      ${day}
+      ${formatForcastDay(forecastDay.time)}
       <br />
       <img
-        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+          forecastDay.condition.icon
+        }.png"
         alt=""
         width="36"
       />
       <br />
-      <span class="weather-forecast-temperature-max">18°</span>
-      <span class="weather-forecast-temperature-min">12°</span>
+      <span class="weather-forecast-temperature-max">${Math.round(
+        forecastDay.temperature.maximum
+      )}°</span>
+      <span class="weather-forecast-temperature-min">${Math.round(
+        forecastDay.temperature.minimum
+      )}°</span>
     </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-displayForecast();
+function getForecast(city) {
+  let apiKey = "1t46b45ad73356d1fdb57b7o025ca415";
+  let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios.get(apiURL).then(displayForecast);
+}
+
 function showWeather(response) {
   let temperature = document.querySelector("h2");
   let currentTemperature = Math.round(response.data.temperature.current);
@@ -69,6 +91,8 @@ function showWeather(response) {
   let currentCity = response.data.city;
   city.innerHTML = currentCity;
   celsiusTemperature = currentTemperature;
+
+  getForecast(response.data.city);
 }
 
 function search(city) {
